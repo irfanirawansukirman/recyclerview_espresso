ab132ecf806a9d8bacf27c8e3e8e9611c156ba5d
irfanirawansukirman <dadang.kotz@gmail.com>
2019-06-27 17:09:12 +0700

    init

---
+++ app/src/androidTest/java/com/example/android/recyclerview/RecyclerViewTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.recyclerview;
+
+
+import android.content.res.Resources;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Recorded Espresso test for adding an item to a RecyclerView.
+ */
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RecyclerViewTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    /**
+     * This test clicks the floating action button and checks to see if a new
+     * item has been added to the RecyclerView with the text "+ Word 20".
+     */
+    @Test
+    public void recyclerViewTest() {
+        /* clicking the item */
+        onView(withId(R.id.recyclerview))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        /* check if the ViewHolder is being displayed */
+        onView(new RecyclerViewMatcher(R.id.recyclerview)
+                .atPositionOnView(0, R.id.word))
+                .check(matches(isDisplayed()));
+
+        /* checking for the text of the first one item */
+        onView(new RecyclerViewMatcher(R.id.recyclerview)
+                .atPositionOnView(0, R.id.word))
+                .check(matches(withText("Word 0")));
+    }
+
+    public class RecyclerViewMatcher {
+
+        private final int recyclerViewId;
+
+        public RecyclerViewMatcher(int recyclerViewId) {
+            this.recyclerViewId = recyclerViewId;
+        }
+
+        public Matcher<View> atPosition(final int position) {
+            return atPositionOnView(position, -1);
+        }
+
+        public Matcher<View> atPositionOnView(final int position, final int targetViewId) {
+            return new TypeSafeMatcher<View>() {
+                Resources resources = null;
+                View childView;
+
+                public void describeTo(Description description) {
+                    String idDescription = Integer.toString(recyclerViewId);
+                    if (this.resources != null) {
+                        try {
+                            idDescription = this.resources.getResourceName(recyclerViewId);
+                        } catch (Resources.NotFoundException var4) {
+                            idDescription = String.format("%s (resource name not found)",
+                                    new Object[]{Integer.valueOf(recyclerViewId)});
+                        }
+                    }
+                    description.appendText("with id: " + idDescription);
+                }
+
+                public boolean matchesSafely(View view) {
+                    this.resources = view.getResources();
+                    if (childView == null) {
+                        RecyclerView recyclerView = (RecyclerView) view.getRootView().findViewById(recyclerViewId);
+                        if (recyclerView != null && recyclerView.getId() == recyclerViewId) {
+                            childView = recyclerView.findViewHolderForAdapterPosition(position).itemView;
+                        } else {
+                            return false;
+                        }
+                    }
+                    if (targetViewId == -1) {
+                        return view == childView;
+                    } else {
+                        View targetView = childView.findViewById(targetViewId);
+                        return view == targetView;
+                    }
+                }
+            };
+        }
+    }
+
+
+}
